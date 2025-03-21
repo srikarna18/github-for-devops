bb816b1b0969c5c782758094593e0b6e4420290d
srikarna18 <141703505+srikarna18@users.noreply.github.com>
2025-03-21 12:46:50 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,9 +1,9 @@
+"""
+This module provides a simple function to print "Hello, World!" to the console.
+"""
 
-  # A simple Python function
-def fun():
-    print("Welcome to GFG")
-
-
-# Driver code to call a function
-fun()
-
+def my_function():
+    """
+    This function prints "Hello, World!" to the console.
+    """
+    print("Hello, World!")
